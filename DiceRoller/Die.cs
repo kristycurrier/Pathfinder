def549815ed66be3a864628b936689e5a546a0c8
Kristy Currier <kristy.currier@tylertech.com>
2019-10-08 14:29:04 -0400

    Added die

---
+++ DiceRoller/Die.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DiceRoller
+{
+    public class Die
+    {
+        public int AmountOfSides { get; set; }
+
+        public string Size { get; set; }
+    }
+}
---
+++ DiceRoller/RandomRollGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DiceRoller
+{
+    public class RandomRollGenerator
+    {
+        public int Roll(Die die)
+        {
+            var random = new Random();
+
+            var roll = random.Next(die.AmountOfSides + 1);
+
+            return roll;
+        }
+
+    }
+}
